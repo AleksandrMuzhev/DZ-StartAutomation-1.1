ce7b447926d779ff679e6297863e3ada6e81202e
Aleksand Muzhev <muzhev1990@mail.ru>
2023-06-16 13:19:00 +0500

    Added test junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,35 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void shouldReturn100IfAmountIs999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn999IfAmountIs1001() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
     }
 }
