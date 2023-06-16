3a5ceb7bfbca2f8130d53e9e9271808cd6a84946
Aleksand Muzhev <muzhev1990@mail.ru>
2023-06-17 02:51:06 +0500

    Edit import annotations Test junit5

--- src/test/java/ru/netology/service/CashbackHackServiceJunit5Test.java
+++ src/test/java/ru/netology/service/CashbackHackServiceJunit5Test.java
@@ -1,11 +1,7 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceJunit5Test {
 
