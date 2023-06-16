7d7c7c1174ec837113d7e390eb120de85d8feec8
Aleksand Muzhev <muzhev1990@mail.ru>
2023-06-16 13:15:44 +0500

    Update gradle junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,12 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void remain() {
+    }
 }
